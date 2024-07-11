4d478056a4b8136bf36a9519ee7cc70e25f03d63
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-11 00:45:51 -0600

    Update vite.config.js

--- proquest/vite.config.js
+++ proquest/vite.config.js
@@ -2,13 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const ports = {
+  applocal:4000,
+  apilocal:8080,
   app:4000,
   api:8080,
 }
 
 const baseURLs = {
-  app:`http://localhost:${ports.app}`,
-  api:`http://localhost:${ports.api}`,
+  applocal:`http://localhost:${ports.applocal}`,
+  apilocal:`http://localhost:${ports.apilocal}`,
+  app:`https://proquest-pspc.onrender.com/`,
+  api:`https://proquest-pspc.onrender.com/`,
 }
 
 // https://vitejs.dev/config/
