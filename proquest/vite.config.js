0eb859d0d1c64b362c595abab23f866d954a1e28
Byvalvin <105598502+Byvalvin@users.noreply.github.com>
2024-07-11 18:28:48 -0600

    Update vite.config.js

--- proquest/vite.config.js
+++ proquest/vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
         target:baseURLs.api,
         changeOrigin:true,
         // rewrite:(path)=>path.replace(/^\/api/,"") // /api maps to https://localhost:7000
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // optional, if you need to rewrite the path
       }
     }
   }
