931e280f2daf59d01c1d06394ea46a040a817c03
joyce omamuli <joycemacbookair2@joyces-MacBook-Air2.local>
2024-07-14 19:20:52 -0600

    render back

--- proquest/src/components/TeamProfiles.jsx
+++ proquest/src/components/TeamProfiles.jsx
@@ -27,7 +27,7 @@ const teamsLoader = async () => {
     try {
         //const allTeamsUrl = "/api/teams"; // Adjust the URL based on your API endpoint for teams
         const baseURLs = ["https://proquest-pspc.onrender.com","https://3b14d84e-bf47-4b87-a7d1-29985604422c-00-373hveltrbpzh.riker.replit.dev:8080"]
-        const allTeamsUrl = `${baseURLs[1]}/api/teams`
+        const allTeamsUrl = `${baseURLs[0]}/api/teams`
         const response = await axios.get(allTeamsUrl);
         return response.data.data; // Assuming your API returns data in a 'data' field
     } catch (error) {
