db1c761e67c6497cc40893735383178847c86b79
joyce omamuli <joycemacbookair2@joyces-MacBook-Air2.local>
2024-07-19 09:31:44 -0600

    Ok so the issue is because i used a 204 response code. IT IS A NO CONTENT response so whatever I tried to send in the response body isnt being sent or something so i cant use it. Just an empty string overwrites whatever is send. The fix right now is to use a 200 reponse code instead on delete. Although unconventional, I want to have access to the list of saved formations after deleting to present it to user

--- proquest/src/pages/PlannerPage.jsx
+++ proquest/src/pages/PlannerPage.jsx
@@ -175,7 +175,7 @@ const PlannerPage = () => {
     const baseURLs = ["https://proquest-pspc.onrender.com","https://3b14d84e-bf47-4b87-a7d1-29985604422c-00-373hveltrbpzh.riker.replit.dev:8080"]
     try {
       const response = await axios.post(`${baseURLs[0]}/api/formations`,{...formation, name})
-      //console.log(response.data)
+      console.log(response.data)
       setFormations(response.data.data)
     } catch (error) {
       console.log(error)
@@ -210,14 +210,12 @@ const PlannerPage = () => {
     const baseURLs = ["https://proquest-pspc.onrender.com","https://3b14d84e-bf47-4b87-a7d1-29985604422c-00-373hveltrbpzh.riker.replit.dev:8080"]
     try {
       const response = await axios.delete(`${baseURLs[0]}/api/formations/${name}`)
-      //console.log(response.data,response.data.data)
+      //console.log(response,response.data,response.data.data,"ggg")
       if(formation.name===name){
         console.log("clear spec")
         handleClearFormation()
       }
-      console.log(formations,"allf")
-      setFormations(response.data.data)
-      console.log(formations,"allf2")
+      //setFormations(response.data.data)
     } catch (error) {
       console.log(error)
     }
