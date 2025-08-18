64d7e045e01be6eb652c545ff599e5a9e0cf41f1
Byvalvin <joycemacbookair2@joyces-MacBook-Air2.local>
2025-08-18 07:16:02 -0600

    an index conundrum

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,5 @@
 import server from '../index.js';
-import { createServer } from 'http';
-import { parse } from 'url';
 
-export default async function handler(req, res) {
-    const vserver = createServer(server);
-    const parsedUrl = parse(req.url, true);
-    req.url = parsedUrl.pathname;
-    vserver.emit('request', req, res);
+export default function handler(req, res) {
+  return server(req, res); // Let Express handle the request directly
 }
