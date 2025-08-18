2359c7877bce1ab3d7cea061a00c80d6274b2148
Byvalvin <joycemacbookair2@joyces-MacBook-Air2.local>
2025-08-18 07:07:18 -0600

    vite vercel config

--- proquest/vite.config.js
+++ proquest/vite.config.js
@@ -1,3 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const ports = {
+  applocal:4000,
+  apilocal:8080,
+  app:4000,
+  api:8080,
+}
+
+const baseURLs = {
+  applocal:`http://localhost:${ports.applocal}`,
+  apilocal:`http://localhost:${ports.apilocal}`,
+  app:"https://proquest-pspc.onrender.com/",
+  api:"https://proquest-pspc.onrender.com/",
+}
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -11,3 +29,4 @@ export default defineConfig({
     }
   }
 })
+
